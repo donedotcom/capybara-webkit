ea4d3beef01da581b3b2acb4425fa096f7ccf7b9
Paul Covell <paul@done.com>
2012-02-26 15:08:31 -0500

    Follow capybara commit to remove Capybara.timeout https://github.com/jnicklas/capybara/commit/6b1a8b61b8c3e05e30b817070a109254af128e97

--- lib/capybara/driver/webkit/browser.rb
+++ lib/capybara/driver/webkit/browser.rb
@@ -1,6 +1,5 @@
 require 'socket'
 require 'thread'
-require 'capybara/util/timeout'
 require 'json'
 
 class Capybara::Driver::Webkit
@@ -194,9 +193,11 @@ class Capybara::Driver::Webkit
     end
 
     def connect
-      Capybara.timeout(5) do
-        attempt_connect
-        !@socket.nil?
+      Timeout.timeout(5) do
+        while @socket.nil?
+          attempt_connect
+          sleep(0.1) if @socket.nil?
+        end
       end
     end
 
